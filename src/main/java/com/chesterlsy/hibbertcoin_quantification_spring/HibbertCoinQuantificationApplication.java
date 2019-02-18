d803f5088eefac5a8d52c5078a38477f6236eabd
Chesterlsy <siyiliu641@hotmail.com>
2019-02-18 18:29:52 +0800

    Initial commit

---
+++ src/main/java/com/chesterlsy/hibbertcoin_quantification_spring/HibbertCoinQuantificationApplication.java
@@ -0,0 +1,13 @@
+package com.chesterlsy.hibbertcoin_quantification_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibbertCoinQuantificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibbertCoinQuantificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chesterlsy/hibbertcoin_quantification_spring/HibbertCoinQuantificationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chesterlsy.hibbertcoin_quantification_spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HibbertCoinQuantificationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
